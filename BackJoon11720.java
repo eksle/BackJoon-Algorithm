d0b8035c940f34dc5a1fe5d4c3bf5f6aa18dafa2
tfesalmbp <tfesal@naver.com>
2021-09-02 00:18:03 +0900

    BackJoon 11720 JAVA

---
+++ BackJoon11720.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+	
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		String a = in.next();
+		in.close();
+		
+		int sum = 0;
+		
+		for(int i = 0; i < N; i++) {
+			sum += a.charAt(i)-'0';
+		}
+		
+		System.out.print(sum);
+
+	}
+	
+}
+
